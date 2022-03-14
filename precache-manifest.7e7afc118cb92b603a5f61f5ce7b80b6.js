0c1a20bcf0e971bddeeb5bc27eb2a47f94c19664
gustlrtbvj <gustlrtbvj@gmail.com>
2022-03-15 00:08:16 +0900

    Updates

---
+++ precache-manifest.7e7afc118cb92b603a5f61f5ce7b80b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6d6640d790fd82a54d54e26d8ff51d",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "0a46c2ebed92cc012756",
+    "url": "/nwitter/static/css/main.1e461c8a.chunk.css"
+  },
+  {
+    "revision": "4bc2b86bfcfa59526dcd",
+    "url": "/nwitter/static/js/2.16702456.chunk.js"
+  },
+  {
+    "revision": "3c45ab1cfcbc2f577ca90e00eda0830a",
+    "url": "/nwitter/static/js/2.16702456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a46c2ebed92cc012756",
+    "url": "/nwitter/static/js/main.0ab59f82.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
