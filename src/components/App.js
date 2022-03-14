e6c045249276a4bfd10fa62299b766714f3c5914
gustlrtbvj <gustlrtbvj@gmail.com>
2022-03-14 15:52:30 +0900

    nweet

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,25 @@ import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import {authService} from "fBase";
 
+
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
+        setUserObj(user);
       }
       setInit(true);
     });
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? (
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+      ) : (
+        "Initializing..."
+      )}
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,73 @@
-import React from "react";
+import { dbService } from 'fBase';
+import { addDoc, collection, getFirestore, onSnapshot, orderBy, query} from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import Nweet from "components/Nweet";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+    const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    useEffect(() => {
+        // 실시간으로 데이터를 데이터베이스에서 가져오기
+        const q = query(
+        collection(getFirestore(), 'nweets'),
+        // where('text', '==', 'hehe') // where뿐만아니라 각종 조건 이 영역에 때려부우면 됨
+        orderBy('createdAt',"desc")
+        );
+        const unsubscribe = onSnapshot(q, querySnapshot => {
+        const newArray = querySnapshot.docs.map(doc => {
+        return {
+        id: doc.id,
+        ...doc.data(),
+        };
+        });
+        setNweets(newArray);
+        console.log('Current tweets in CA: ', newArray);
+        });
+        return () => {
+        unsubscribe();
+        };
+        }, []);
+        
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await addDoc(collection(dbService, "nweets"), {
+        text: nweet,
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+        });
+        setNweet("");
+        };
+    const onChange = (event) => {
+      const {
+        target: { value },
+      } = event;
+      setNweet(value);
+    };
+
+
+
+    return (
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            value={nweet}
+            onChange={onChange}
+            type="text"
+            placeholder="What's on your mind?"
+            maxLength={120}
+          />
+          <input type="submit" value="Nweet" />
+        </form>
+        <div>
+        {nweets.map((nweet) => (
+          <Nweet
+          key={nweet.id}
+          nweetObj={nweet}
+          isOwner={nweet.creatorId === userObj.uid}
+        />
+        ))}
+      </div>
+      </div>
+    );
+  };
 export default Home; 
