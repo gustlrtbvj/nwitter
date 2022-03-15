1ebecd0104f74758a6cd5316c54af74bedcb3254
gustlrtbvj <gustlrtbvj@gmail.com>
2022-03-15 19:19:54 +0900

    오류수정

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -138,15 +138,6 @@ const NweetFactory  = ({ userObj }) => {
           </div>
         )}
         </form>
-        <div>
-        {nweets.map((nweet) => (
-          <Nweet
-          key={nweet.id}
-          nweetObj={nweet}
-          isOwner={nweet.creatorId === userObj.uid}
-          />
-        ))}
-      </div>
       </div>
     );
   };
